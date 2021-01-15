32d66692892ff6b0d1aee5735d1bdf549e65c0ce
ruvick <rudikov.web@ya.ru>
2021-01-16 00:31:02 +0300

    Дополнения к wp-php

--- wp-info/info-wp.php
+++ wp-info/info-wp.php
@@ -1,4 +1,10 @@
 
+<!-- Подключения template-parts -->
+<?php get_template_part('template-parts/header-cat');?>
+<!-- =========================================================================================================================================== -->
+
+
+
 <!-- Подключения страниц, категорий -->
 <a href="<?php echo get_permalink(25);?>">Смотреть квалификацию</a> <!-- Подключение страниц -->
 
@@ -122,13 +128,16 @@ function wp_corenavi() {
 
 
 <!-- Вывод определенных записией в любом месте -->
+<!-- https://wp-kama.ru/function/get_posts#include -->
 <?php 
 $posts = get_posts( array(
 	'numberposts' => 3,
 	'category'    => 5,
-	'orderby'     => '612,616,626',
+	'orderby'     => 'date',
+	// 'orderby'     => '612,616,626',
 	'order'       => 'DESC',
-	'include'     => '612,608,606',
+	// 'include'     => '612,608,606',
+	'include'     => array(),
 	'exclude'     => array(),
 	'meta_key'    => '',
 	'meta_value'  =>'',
@@ -149,11 +158,122 @@ foreach( $posts as $post ){
 						</div>
 						<div class="materials__text">
 							<h3><?php echo $post->post_title?></h3>
-							<p><?php the_content(); ?></p>
+							<p><?php the_excerpt(); ?></p> <!-- 55 символов -->
 							<a href="<?php echo get_permalink();?>" class="materials__btn btn">Подробнее</a>
 						</div>
 					</div>
 				</a>	<?php 
 } 
 ?>
+
+<?php the_excerpt(); ?> - настраиваемая функция
 <!-- =========================================================================================================================================== -->
+
+
+
+
+<!-- Вывод Заголовка страницы из админки -->
+		<h1 class="header-services__title">
+			<?php the_title();?>
+		</h1>
+
+
+<!-- Вывод Заголовка категорий из админки -->
+		<h1 class="header-services__title">
+			<?php echo get_queried_object()->name;?>
+		</h1>
+
+
+<!-- Вывод текста страницы из админки -->
+<?php the_content(); ?>
+
+<!-- Вывод части текста записи из админки -->
+<?php the_excerpt(); ?>
+<!-- =========================================================================================================================================== -->
+
+
+
+<!-- Добавляем разные фишки к выводу изображений. Превьюшки и т.д.-->
+<!-- Ссылка - https://wp-kama.ru/function/add_theme_support -->
+add_theme_support()
+
+
+
+<!-- Установим картинку Шапки по умолчанию и дадим возможность изменять её пользователям в настройках темы -->
+<img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
+add_theme_support( 'custom-header', array(
+	'width'         => 1070,
+	'height'        => 758,
+	'default-image' => get_template_directory_uri() . '/img/header-02.jpg',
+	'uploads'       => true,
+) );
+<!-- =========================================================================================================================================== -->
+
+
+
+<!-- Позволяет регестрировать свои миниатюры изображений -->
+add_image_size()
+<!-- =========================================================================================================================================== -->
+
+
+
+<!-- Добавление "Цитаты" для страниц -->
+function page_excerpt() {
+add_post_type_support('page', array('excerpt'));
+}
+add_action('init', 'page_excerpt');
+
+Вывод на странице
+		<div class="header-services-sub">
+			<?php the_excerpt(); ?>
+		</div>
+<!-- =========================================================================================================================================== -->
+
+
+
+<!-- Вывод хлебных крошек из плагина Yoast SEO -->
+		<div class="header-copy__breadcrumb">
+			<?php
+			if (function_exists ('yoast_breadcrumb')) {
+				yoast_breadcrumb ('<p id = "breadcrumbs">', '</p>');
+			}
+			?>
+		</div>
+<!-- ============================================================================================================================================ -->
+
+
+
+
+<!-- Вывод e-mail из поля карбон -->
+<a href="mailto:<? echo $mail = carbon_get_theme_option("as_email"); ?>" class="mobile-mail contact-mail"><? echo $mail; ?></a>
+
+
+<!-- Вывод телефона из поля карбон -->
+<a href="tel:<? echo preg_replace('/[^0-9]/', '', $tel); ?>" class="contact-tel"><? echo $tel = carbon_get_theme_option("as_phones_1"); ?></a>
+<!-- ============================================================================================================================================ -->
+
+
+
+<!-- Вывод картинки из админки страницы в шапку или в любое место -->
+
+<!-- Создаем поле в carbon -->
+Container::make('post_meta', 'resort_city', 'Доп. поля')
+// ->show_on_template('page-services.php')
+->add_fields(array(
+  Field::make('image', 'resort_banner', 'Фото баннера')
+  ->help_text( 'Изображение не менее 1070 х 758px'),
+));
+
+<!-- Пишем php-код на странице где выводим картинку -->
+<?php 
+	$banner = wp_get_attachment_image_src( carbon_get_the_post_meta('resort_banner'), 'full')[0];
+		if(empty($banner)) {
+	$banner = get_template_directory_uri() . 'img/header-01.jpg';
+} ?>
+
+<!-- Выводим картинку в img -->
+	<img src="<?php echo $banner?>" alt="">
+
+<!-- Выводим картинку фоном в блоке -->
+	<div class="header-services__img" style="background-image: url(<?php echo $banner?>);"></div>
+<!-- ============================================================================================================================================ -->
